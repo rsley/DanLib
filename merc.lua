83c02526e13c7b72a22e25fdb2d073cbb7934c5d
Rafael Soley R. <rafaelsoley.multiple@gmail.com>
2023-12-30 21:35:39 +0000

    Fix Repo (2)

--- merc.lua
+++ merc.lua
@@ -908,7 +908,7 @@ function Library:create(options)
 
 	rawset(mt, "creditsContainer", creditsTab.container)
 
-	creditsTab:credit{Name = "IDEalistic", Description = "Developer", Discord = "idealistical", Github = "https://github.com/rsley/"}
+	creditsTab:credit{Name = "IDEalistic", Description = "Developer", Discord = "idealistical", Github="https://github.com/rsley/"}
 	creditsTab:credit{Name = "Abstract", Description = "UI Library Developer", Discord = "Abstract#8007", V3rmillion = "AbstractPoo"}
 	creditsTab:credit{Name = "Deity", Description = "UI Library Developer", Discord = "Deity#0228", V3rmillion = "0xDEITY"}
 	creditsTab:credit{Name = "Repository", Description = "UI Library", Github="https://github.com/rsley/DanLib/blob/main/merc.lua"}
